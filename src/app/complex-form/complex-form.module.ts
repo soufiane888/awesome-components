a3125d868b449fe36f29a492baa9862e5752fa2d
SADDIK <soufiane.saddik@sogeti.com>
2023-07-31 20:29:00 +0200

    complex-form

--- src/app/complex-form/complex-form.module.ts
+++ src/app/complex-form/complex-form.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ComplexFormRoutingModule } from './complex-form-routing.module';
+import { ComplexFormComponent } from './components/complex-form/complex-form.component';
+import { SharedModule } from '../shared/shared.module';
+import { ComplexFormService } from './services/complex-form.service';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ComplexFormComponent
+  ],
   imports: [
     CommonModule,
-    ComplexFormRoutingModule
+    ComplexFormRoutingModule,
+    SharedModule
+  ],
+  providers: [
+    ComplexFormService
   ]
 })
 export class ComplexFormModule { }
+
---
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-complex-form',
+  templateUrl: './complex-form.component.html',
+  styleUrls: ['./complex-form.component.scss']
+})
+export class ComplexFormComponent implements OnInit {
+
+  mainForm!: FormGroup;
+  personalInfoForm!: FormGroup;
+  contactPreferenceCtrl!: FormControl;
+  emailCtrl!: FormControl;
+  confirmEmailCtrl!: FormControl;
+  emailForm!: FormGroup;
+  phoneCtrl!: FormControl;
+  passwordCtrl!: FormControl;
+  confirmPasswordCtrl!: FormControl;
+  loginInfoForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initFormControls();
+    this.initMainForm();
+  }
+
+  private initMainForm(): void {
+    this.mainForm = this.formBuilder.group({
+        personalInfo: this.personalInfoForm,
+        contactPreference: this.contactPreferenceCtrl,
+        email: this.emailForm,
+        phone: this.phoneCtrl,
+        loginInfo: this.loginInfoForm
+    });
+}
+
+private  initFormControls(): void{
+  this.personalInfoForm = this.formBuilder.group({
+    firstname:['', Validators.required],
+    lastname:['', Validators.required],
+  });
+  this.contactPreferenceCtrl = this.formBuilder.control('email');
+  this.emailCtrl = this.formBuilder.control('');
+  this.confirmEmailCtrl = this.formBuilder.control('');
+  this.emailForm = this.formBuilder.group({
+    email: this.emailCtrl,
+    confirm: this.confirmEmailCtrl
+  })
+  this.phoneCtrl = this.formBuilder.control('');
+  this.passwordCtrl = this.formBuilder.control('', Validators.required);
+  this.confirmPasswordCtrl = this.formBuilder.control('', Validators.required);
+  this.loginInfoForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    upassword: this.passwordCtrl,
+    confirmPasword: this.confirmPasswordCtrl
+  })
+
+}
+onSubmitForm() {
+  console.log(this.mainForm.value);
+}
+}

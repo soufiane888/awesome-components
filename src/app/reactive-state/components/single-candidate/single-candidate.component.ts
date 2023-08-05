7793566bc6eac0365d773162f305519d978516a5
SADDIK <soufiane.saddik@sogeti.com>
2023-08-05 21:00:32 +0200

    affichage d'un seul candidat

--- src/app/reactive-state/components/single-candidate/single-candidate.component.ts
+++ src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -1,4 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+import { CandidatesService } from '../../services/candidates.service';
+
+
 
 @Component({
   selector: 'app-single-candidate',
@@ -7,9 +13,31 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SingleCandidateComponent implements OnInit{
-constructor(){}
+
+loading$!: Observable<boolean>;
+candidate$!: Observable<Candidate>;
+
+constructor(private candidatesService: CandidatesService,
+  private route: ActivatedRoute,
+  private router: Router){}
 
 ngOnInit(): void {
-  
+ this.initObservable();
 }
+
+private initObservable() {
+    this.loading$ = this.candidatesService.loading;
+    this.candidate$ = this.route.params.pipe(
+      switchMap(params => this.candidatesService.getCandidateById(+params['id']))
+    )
+  }
+  onHire(){
+
+  }
+  onRefuse(){
+
+  }
+  onGoBack(){
+this.router.navigateByUrl('/reactive-state/candidates');
+  }
 }
--- src/app/reactive-state/services/candidates.service.ts
+++ src/app/reactive-state/services/candidates.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, delay, Observable, tap } from 'rxjs';
+import { BehaviorSubject, delay, map, Observable, tap } from 'rxjs';
 import { Candidate } from '../models/candidate.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class CandidatesService {
-    constructor(private http: HttpClient){}
+
+ constructor(private http: HttpClient){}
  
  private _loading$ = new BehaviorSubject<boolean>(false);
  get loading(): Observable<boolean>{
@@ -21,14 +22,28 @@ export class CandidatesService {
     this._loading$.next(loading);
   }
 
+  private lastCandidatesLoad = 0;
 getCandidatesFromServer(){
+  if(Date.now() - this.lastCandidatesLoad <= 300000){
+    return;
+  }
   this.setLoadingStatus(true);
   this.http.get<Candidate[]>(`${environment.apiUrl}/candidates`).pipe(
     delay(1000),
     tap(candidates => {
+      this.lastCandidatesLoad = Date.now();
        this._candidates$.next(candidates);
        this.setLoadingStatus(false);
     })
   ).subscribe();
 }  
+
+getCandidateById(id: number): Observable<Candidate>{
+  if(!this.lastCandidatesLoad){
+    this.getCandidatesFromServer();
+  }
+  return this.candidates$.pipe(
+    map(candidates => candidates.filter(candidate => candidate.id === id)[0])
+  );
+}
 }
